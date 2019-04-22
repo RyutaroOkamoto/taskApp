a6da1f32d711bfc9bac08163743981bfad4a5981
ryutaro <ryuryu.0910@icloud.com>
2019-04-22 21:57:24 +0900

    pod install と .gitignoreまでの作成完了

--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    //
+    func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    //
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:
+            indexPath)
+        return cell
+    }
+    //cellをtapした時に行われるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
+    //cellが削除可能を伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)->        UITableViewCell.EditingStyle{
+    return .delete
+    }
+    //deleteを押した時に出る
+    func tableView(_ tableView: UITableView, commit eddingStyle: UITableViewCell.EditingStyle, forRowAt IndexPath: IndexPath){
+        
+    }
+ 
+    
+    
 
 }
 
